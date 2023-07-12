efde0ea17b40ced725dff05537c1e38f5a397179
gilbenhamo <gilbh859@gmail.com>
2023-07-12 21:21:36 +0300

    HeroText finished

---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+import { styles } from "../styles";
+
+const Hero = () => {
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start justify-center gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-green-300" />
+          <div className="w-1 sm:h-80 h-40 green-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm
+            <span className=" text-green-300"> Gil</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Recently gradute software engineer, <br className="sm:block hidden"/>
+            Full stack developer 
+          </p>
+        </div>
+      </div>
+      
+    </section>
+  );
+};
+
+export default Hero;
--- src/styles.ts
+++ src/styles.ts
@@ -3,8 +3,9 @@ const styles = {
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
   
+    paddingHero:"lg:px-72 md:px-52 sm:px-16 px-6",
     heroHeadText:
-      "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+      "font-black text-white lg:text-[70px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
     heroSubText:
       "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
   
