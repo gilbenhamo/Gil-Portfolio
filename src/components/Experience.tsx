ae5a546f54cf3185f67e75726cf8efd6983641a9
gilbenhamo <gilbh859@gmail.com>
2023-07-15 11:46:21 +0300

    Start vertical time line

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,15 +1,25 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { experiences } from "../constants";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import SectionHead from "./UI/SectionHead";
 import { SectionWrraper } from "./hoc/SectionWrraper";
+import ExperienceCard from "./UI/ExperienceCard";
+import 'react-vertical-timeline-component/style.min.css';
+
 
 const Experience = () => (
-    <>
-        <SectionHead pText={"Introduction"} headText={"Overview."} />
-        <div className="mt-20 flex flex-col">
-        </div>
-    </>
+  <>
+    <SectionHead pText={"Introduction"} headText={"Overview."} />
+    <div className="mt-20 flex flex-col">
+      <VerticalTimeline>
+      {experiences.map((e, index) => (
+
+         <ExperienceCard key={index}/>
+        ))}
+      </VerticalTimeline>
+    </div>
+  </>
 );
 
-export default SectionWrraper(Experience,'work');
+export default SectionWrraper(Experience, "work");
---
+++ src/components/UI/ExperienceCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { VerticalTimelineElement } from 'react-vertical-timeline-component'
+
+const ExperienceCard = () => {
+  return (
+    <VerticalTimelineElement
+    // contentStyle={{background:'#1d1836', color:'#fff'}}
+    // contentArrowStyle={{borderRight:'tpx solid #232631'}}
+    // date={'1/1/1111'}
+    // icon={
+    //     <div>
+    //         <img/>
+    //     </div>
+    // }
+    >
+        <div>
+            <h3>
+                asdadad
+            </h3>
+        </div>
+    </VerticalTimelineElement>
+  )
+}
+
+export default ExperienceCard
